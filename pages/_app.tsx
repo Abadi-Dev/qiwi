646a0503358a93acb68dc414d7b696cf3024ae38
Abdulelah Alamri <Abadi_dev@outlook.com>
2022-11-21 01:08:40 +0300

    remove tremor imports for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import "@tremor/react/dist/esm/tremor.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function Myapp({ Component, pageProps }: AppProps) {
